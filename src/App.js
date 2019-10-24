cb7ef17d7dd7615413e66b53eb184a6345f2dc3c
ZEMA <mzemni@octo.com>
2019-10-24 15:47:42 +0200

    premier commit qd meme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState }  from 'react';
 import './App.css';
 
-function App() {
+
+import axios from 'axios';
+
+
+export function BeerOfTheDay() 
+{
+  const [beer, setBeer] = useState();
+
+  useEffect(() => 
+  {
+    axios.get('https://api.punkapi.com/v2/beers/random')
+      .then(beer => {
+        console.log(beer);
+
+        
+        let newBeer = {
+          name: beer.data[0].name,
+          image: beer.data[0].image_url
+        };
+
+        setBeer(newBeer)
+      })
+
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bierre du jour</h1>
+      {beer ? (<p>{beer.name}</p>) : "rien"}
+
+      <img src={(beer && beer.image) && beer.image} alt="a beer pic"></img>
+      
+
     </div>
   );
 }
-
-export default App;
